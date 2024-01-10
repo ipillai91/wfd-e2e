a0260dc42d826c748bbfa9aadc99725fb9f4c2cb
ipillai91 <98965503+ipillai91@users.noreply.github.com>
2024-01-10 14:17:47 +0530

    Update AvailabilityResource.java

--- where-for-dinner-availability/src/main/java/com/java/example/tanzu/wherefordinner/resources/AvailabilityResource.java
+++ where-for-dinner-availability/src/main/java/com/java/example/tanzu/wherefordinner/resources/AvailabilityResource.java
@@ -155,6 +155,6 @@ public class AvailabilityResource
 	@GetMapping("app/version")
 	public Mono<String> getAvailabilityAppVersion()
 	{
-		return Mono.just("ipillai 8:30 am jan 10");
+		return Mono.just("ipillai 2:30 pm jan 10");
 	}
 }
